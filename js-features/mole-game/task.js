4cd563217930efd537fea7d46c9089f2c8df3b8d
Anna Bolgova <97449480+Hannellee@users.noreply.github.com>
2022-08-10 16:24:53 +0300

    changed 2

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -6,28 +6,31 @@ for (let i = 0; i < holes.length; i++) {
     
     holes[i].onclick = function () {
 
-        let dead = document.getElementById('dead');
-        let lost = document.getElementById('lost');
-
-
-        if (hole.className.includes('hole_has-mole')) {
-            dead.textContent = String(+(dead.textContent) + 1);
-        } else {
-            lost.textContent = String(+(lost.textContent) + 1);
+        for (let j = 0; j < hole.length; j++) {
+            let dead = document.getElementById('dead');
+            let lost = document.getElementById('lost');
+    
+    
+            if (hole[j].className.includes('hole_has-mole')) {
+                dead.textContent = String(+(dead.textContent) + 1);
+            } else {
+                lost.textContent = String(+(lost.textContent) + 1);
+            }
+    
+            if (+dead.textContent === 10 && +lost.textContent < 5) {
+                alert('Поздравляем! Вы победили!');
+                lost.textContent = '0';
+                dead.textContent = '0';    
+            } else if(+dead.textContent < 10 && +lost.textContent >= 5) {
+                alert('Упс! Вы проиграли!');
+                lost.textContent = '0';
+                dead.textContent = '0';    
+            } else {
+                return;
+            }
+    
         }
-
-        if (+dead.textContent === 10 && +lost.textContent < 5) {
-            alert('Поздравляем! Вы победили!');
-            lost.textContent = '0';
-            dead.textContent = '0';    
-        } else if(+dead.textContent < 10 && +lost.textContent >= 5) {
-            alert('Упс! Вы проиграли!');
-            lost.textContent = '0';
-            dead.textContent = '0';    
-        } else {
-            return;
-        }
-
+            
     }
 }
 
