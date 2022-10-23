2a97af8492af2d01b9858daaf6db1f9cb7bc84b9
Anna Bolgova <97449480+Hannellee@users.noreply.github.com>
2022-10-23 16:12:11 +0300

    second commit

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,14 +1,24 @@
-let sliderArrowPrev = document.querySelector('slider__arrow_prev');
-let sliderArrowNext = document.querySelector('slider__arrow_next');
-let sliderItem = document.querySelectorAll('slider__item');
+let sliderArrowPrev = document.querySelector('.slider__arrow_prev');
+let sliderArrowNext = document.querySelector('.slider__arrow_next');
+let sliderItem = document.querySelectorAll('.slider__item');
 
 let sliderItemArray = Array.from(sliderItem);
+let sliderIndex = 0;
 
 let changePrevSliderItem = function () {
-    for (let i = 0; i < sliderItemArray.length; i++) {
-        sliderItemArray[i].classList.remove('slider__item_active');
+    for (let i = sliderItemArray.length; i > 0; i--) {
+        let activeSlideIndex = sliderItemArray.findIndex(item => item.classList.contains('slider__item_active'));
+
+        if (activeSlideIndex <= 1) {
+            i = sliderItemArray.length;
+        }
+
+
+        sliderItemArray[activeSlideIndex].classList.remove('slider__item_active');
+        sliderItemArray[activeSlideIndex -1].classList.add('slider__item_active');
+        
     }
 }
 
-sliderArrowPrev.onclick = changePrevSliderItem();
+sliderArrowPrev.onclick = changePrevSliderItem;
 
