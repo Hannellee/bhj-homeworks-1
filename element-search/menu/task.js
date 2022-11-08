e03deb1b77bf7cf63a9f3b4f98e369b6f3dd853a
Anna Bolgova <97449480+Hannellee@users.noreply.github.com>
2022-11-08 15:38:40 +0300

    new commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,18 +1,12 @@
-// let menuLink = document.querySelectorAll('.menu__link');
-// let menuSub = document.querySelectorAll('.menu_sub');
-
 let menuLinksArray = Array.from(document.querySelectorAll('.menu__link'));
 let menuSubArray = Array.from(document.querySelectorAll('.menu_sub'));
 
 for (let i = 0; i < menuLinksArray.length; i++) {
 
     let showSubMenu = function () {
-        let subLinkItem = menuLinksArray[i].closest('.menu_sub')
-        if (subLinkItem !== null) {
-            for (let j = 0; j < menuSubArray.length; j++) {
-                menuSubArray[j].classList.add('menu_active')                
-            }
-            
+        if (this.nextElementSibling.tagName === 'UL') {
+            this.nextElementSibling.classList.add('menu_active');
+
             return false;
         } else {
             return true;
