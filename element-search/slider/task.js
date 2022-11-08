6ce9d84af7c7afcd1dcb00a4bc98f2e9b39726a4
Anna Bolgova <97449480+Hannellee@users.noreply.github.com>
2022-11-08 15:39:46 +0300

    new commit

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,24 +1,35 @@
 let sliderArrowPrev = document.querySelector('.slider__arrow_prev');
 let sliderArrowNext = document.querySelector('.slider__arrow_next');
-let sliderItem = document.querySelectorAll('.slider__item');
+let sliderItem = Array.from(document.querySelectorAll('.slider__item'));
 
-let sliderItemArray = Array.from(sliderItem);
 let sliderIndex = 0;
 
-let changePrevSliderItem = function () {
-    for (let i = sliderItemArray.length; i > 0; i--) {
-        let activeSlideIndex = sliderItemArray.findIndex(item => item.classList.contains('slider__item_active'));
 
-        if (activeSlideIndex <= 1) {
-            i = sliderItemArray.length;
-        }
-
-
-        sliderItemArray[activeSlideIndex].classList.remove('slider__item_active');
-        sliderItemArray[activeSlideIndex -1].classList.add('slider__item_active');
-        
+sliderArrowNext.onclick = () => {
+    if (sliderIndex === sliderItem.length - 1) {
+        sliderIndex = 0;
+    } else {
+        sliderIndex++
     }
+
+    changeSliderImage(sliderIndex);
 }
 
-sliderArrowPrev.onclick = changePrevSliderItem;
+sliderArrowPrev.onclick = () => {
+    if (sliderIndex === 0) {
+        sliderIndex = sliderItem.length - 1;
+    } else {
+        sliderIndex--
+    }
 
+    changeSliderImage(sliderIndex);
+}
+
+let changeSliderImage = function (idx) {
+    
+    for (let i = 0; i < sliderItem.length; i++) {
+        sliderItem[i].classList.remove('slider__item_active');
+    }
+
+    sliderItem[idx].classList.add('slider__item_active');
+}
