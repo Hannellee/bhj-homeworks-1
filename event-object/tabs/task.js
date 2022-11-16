67aa29e8378b6993fa6af58fd10b87230454b74e
Anna Bolgova <97449480+Hannellee@users.noreply.github.com>
2022-11-16 21:27:38 +0300

    new commit

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -2,18 +2,18 @@ let tabs = Array.from(document.querySelectorAll('.tab'));
 let tabContent = Array.from(document.querySelectorAll('.tab__content'));
 
 
-let tabIndex = tabs.findIndex((idx) => {
-    tabs[idx].classList.contains('tab_active');    
-})
 
-let tabChanger = function () {
-    for (let i = 0; i < tabs.length; i++) {
+for (let i = 0; i < tabs.length; i++) {
 
-        tabs[i].classList.remove('tab_active');
-        tabContent[i].classList.remove('tab__content_active');
-
+    let changeTabs = function () {
+    
+        tabs.forEach(item => item.classList.remove('tab_active'));
+        tabContent.forEach(item => item.classList.remove('tab__content_active'));
+        
+        tabs[i].classList.add('tab_active');
+        tabContent[i].classList.add('tab__content_active')
     }
 
-    tabs[tabIndex].classList.add('tab_active');
-    tabContent[tabIndex].classList.add('tab__content_active')
+    tabs[i].addEventListener('click', changeTabs);
 }
+
